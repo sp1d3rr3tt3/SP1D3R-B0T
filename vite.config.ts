ab72cc4a4b327839a1b180c940532e4005671c28
sp1d3rr3tt3 <spyderette@kultofspyderrette.com>
2026-04-13 08:06:21 -0700

    Add project files

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import neon from './neon-vite-plugin.ts'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    neon,
+    netlify(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
+
+export default config
